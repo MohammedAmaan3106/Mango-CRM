77f5fb06ac1e981e92eaba7700fd3f12f8c623f3
MohammedAmaan3106 <amaanmohammed757@gmail.com>
2026-07-01 21:07:28 +0530

    Updated database configuration for deployment

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,16 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "amaan@2007",
-  database: "infinitycrm"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
   if (err) {
-    console.log("Database connection failed");
+    console.log("Database connection failed:", err);
     return;
   }
 
